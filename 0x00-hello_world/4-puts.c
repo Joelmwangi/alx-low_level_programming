9bfaa82134b821a9e0e01266cdda21d488dc57e4
Joelmwangi <joelmwangi515@gmail.com>
2022-09-12 11:20:18 -0700

    calll

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-echo 'gcc $CFILE -o cisfun' 
+Â#include <stdio.h>
+
+/*
+* main - print the string in the put function 
+*
+*Description prints programming is like building a multillingual puzzele
+*Return: 0
+*/
+int main(void)
+{
+        puts("\"programming is like buliding a multillingual );
+        return (0);

6bfc372859194a81a3974d71296dbcf0a1ad7fb2
Joelmwangi <joelmwangi515@gmail.com>
2022-09-14 12:28:30 -0700

    callt

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,12 +6,11 @@
  * Return: 0
  */
 int _islower(int c)
-{
-       if (c >= 'a' && c <= 'z')
-
-       {
-                return (1);
+{       
+         if (c >= 'a' && c <= 'z')
+         {
+                  return (1);
         }
         else
-                return (0);
+                  return (0);
 }

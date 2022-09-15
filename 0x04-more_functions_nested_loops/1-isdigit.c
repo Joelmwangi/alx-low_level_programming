44037f351d7485a60ae888e9858d6e54457a3ce9
Joelmwangi <joelmwangi515@gmail.com>
2022-09-15 09:03:19 -0700

    code

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/** 
+ * _isdigit - checks wheather a charcter is a digit or not
+ * @c: tested charcter
+ * Return: 1 if it is, 0 otherwise
+ */
+
+int _isdigit(int c)
+ {
+	 if ((c >= 48) && (c <= 57))
+	 {
+		 return (1);
+	 }
+
+	 return (0);
+
+}

dc6b4e992c6109b47cb3c87754bf332f476a0256
Joelmwangi <joelmwangi515@gmail.com>
2022-09-21 23:59:48 -0700

    code

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,9 +10,9 @@
 char *_strncat(char *dest, char *src, int n)
 {
 
-        int index = 0, dest_len = 0;
+int index = 0, dest_len = 0;
 
-        while (dest[index++])
+while (dest[index++])
                 dest_len++;
 
         for (index = 0; src[index] && index < n; index++)
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,9 +14,10 @@ char *_strcar(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 char *string_toupper(char *);
+char *cap_string(char *);
 char *leet(char *)
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-char *add_strings(char *n1, char*n2, char *r, int r index);
+char *add_strings(char *n1, char*n2, char *r, int r_index);
 
 #endif /*MAIN_H*/

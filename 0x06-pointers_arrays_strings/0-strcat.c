9f3eed2f312388f506ae2d018f347ec7c2d2aead
Joelmwangi <joelmwangi515@gmail.com>
2022-09-21 23:26:59 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,15 +9,15 @@
 * Return: returns poiner to @dest
 */
 
-char *_strcat(char*dest, char*src)
+char *_strcat(char *dest, char *src)
 {
 
 int index = 0, dest_len = 0;
 
-	while(dest[index++])
+	while (dest[index++])
 		dest_len++;
 
-	for(index = 0; src[index]; index++)
+	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
 
 	return (dest);

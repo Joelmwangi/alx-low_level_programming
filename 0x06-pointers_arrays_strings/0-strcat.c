bbe247706462279c70e3871fbc52233961415c6b
Joelmwangi <joelmwangi515@gmail.com>
2022-09-22 01:33:05 -0700

    me code

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,23 +1,22 @@
 #include "main.h"
 
 /**
-* _strcat -> this is a function strcat
-* @dest: first param
-* @src: second param
-* Return: a string
+*_strcat - concatenates  the string pointed to by @src to
+*the end of the string pointed to by @dest
+*@dest: string that will be appended
+*@src: string to be concatenated upon
+*
+*Return: returns poiter to @dest
 */
 char *_strcat(char *dest, char *src)
 {
-    int len = 0, i;
-   
-    while (dest[len])
-	len++;
+	int index = 0, dest_len = 0;
 
-    for (i = 0; src[i] != '\0'; i++)
-    {
-        dest[len] = src[i];
-        len += 1;
-    }
-    dest[len] = '\0';
-    return (dest);
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -18,6 +18,6 @@ char *cap_string(char *);
 char *leet(char *)
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-char *add_strings(char *n1, char*n2, char *r, int r_index);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 
 #endif /*MAIN_H*/

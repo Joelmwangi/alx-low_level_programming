76da6545228ee659547fda031e1bae52b04fec28
Joelmwangi <111023299+Joelmwangi@users.noreply.github.com>
2022-12-09 22:46:31 +0300

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,22 @@
-#include <stdio.h>
+include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 /**
- * main - print if the number is positive , zero , or negative
- *
- * Description: using the main function
- * this program prints 'programming is positive, zero, or negative
+ * main - main block
+ * Description: prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
  * Return: 0
  */
 int main(void)
 {
-char c;
-for (c = 'z'; c >= 'a'; c--)
-{
-putchar(c);
-}
-putchar('\n');
-return (0);
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+
+	putchar('\n');
+	return (0);
 }
